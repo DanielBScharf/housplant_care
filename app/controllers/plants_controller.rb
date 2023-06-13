58c185747506488d4dd178cbe039d6edddc9931a
Daniel Scharf <id.scharfd@gmail.com>
2023-06-13 10:42:27 +0900

    added api stuff but still need to test it, going to set up views and test creating a house before I start adding plants

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,4 +1,5 @@
 class PlantsController < ApplicationController
+
   def index
     @plants = Plant.all
     if params[:query].present?
@@ -7,6 +8,10 @@ class PlantsController < ApplicationController
   end
 
   def show
+    @plants = Plant.all
+    if params[:query].present?
+      @plants = @plants.search(params[:query])
+    end
     @plant = Plant.find(params[:id])
   end
 
@@ -36,6 +41,25 @@ class PlantsController < ApplicationController
   end
 
   private
+
+  def find_plants
+    plants = []
+    key = ENV.call('API_Key')
+    url = "https://perenual.com/api/species-list?page=1&key=#{key}"
+    response = RestClient.get(url)
+    data = JSON.parse(response)
+    plants = data['plants']
+    plants.each do |plant|
+      plants << { scientific_name: plant["scientific_name"],
+                  id: plant['id'],
+                  common_name: plant["commong_name"],
+                  other_name: plant["other_name"],
+                  watering: plant["watering"],
+                  sunlight: plant["sunlight"],
+                  image: "https://perenual.com/storage/species_image/#{id}_#{scientific_name}/og/49255769768_df55596553_b.jpg"}
+    end
+    plants
+  end
 
   def plant_params
     params.require(:plant).permit(:nickname, :watering, :name, :common_name, :scientific_name, :cycle, :watering, :sunlight, :image)
